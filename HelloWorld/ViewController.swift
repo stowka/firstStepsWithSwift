6ae6bfe5964875dc802480ed86f2999fc2318de1
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-10-18 15:10:16 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Antoine De Gieter on 18/10/14.
+//  Copyright (c) 2014 Net production Köbe & co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
