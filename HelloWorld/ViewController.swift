22ef45aa5ef9f0d66143a2028d0ee39cc63cb6c1
Antoine De Gieter <antoine.degieter@net-production.ch>
2014-10-18 16:16:52 +0200

    Action triggered by text field

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var NameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func helloWorldAction(nameTextField: UITextField) {
+    }
 
 }
 
